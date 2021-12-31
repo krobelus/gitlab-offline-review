0bab8de4829ec11946f026e46f4e97cf06a7a4e5
Johannes Altmanninger <aclopte@gmail.com>
2021-12-31 04:36:22 +0100

    fix github branch heuristic

--- gl.py
+++ gl.py
@@ -1258,7 +1258,7 @@ def cmd_discuss(branch, commit, old_file, new_file, line_type, old_line,
     if on_commit:
         pass
     else:
-        if GITHUB and THE_REPOSITORY.git().branch("--list", f"{REMOTE_NAME}/{branch}"):
+        if GITHUB and not THE_REPOSITORY.git().branch("--list", "--remotes", f"{REMOTE_NAME}/{branch}"):
             pr_branch = THE_REPOSITORY.git().for_each_ref(f'--points-at={branch}', f'refs/remotes/{REMOTE_NAME}/pr-*')
             if pr_branch:
                 branch = os.path.basename(pr_branch)
